5efe5ef340dd0f3a87ce914e0916920e267f01c0
YangMun <yang4867@naver.com>
2024-06-19 14:59:37 +0900

    Initial Commit

---
+++ SnakeBlockGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnakeBlockGame
+//
+//  Created by 양문경 on 6/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnakeBlockGame/SnakeBlockGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeBlockGameApp.swift
+//  SnakeBlockGame
+//
+//  Created by 양문경 on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SnakeBlockGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
